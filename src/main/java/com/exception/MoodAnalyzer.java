88354a15be4cae35e24aec991538191c47efc487
aajisahu <aajisahu.com>
2021-10-14 22:44:17 +0530

    Added:reapeting test case 1.1

--- src/main/java/com/exception/MoodAnalyzer.java
+++ src/main/java/com/exception/MoodAnalyzer.java
@@ -3,22 +3,17 @@ package com.exception;
 public class MoodAnalyzer {
     private String message;
 
+
     public MoodAnalyzer() {
     }
 
     public MoodAnalyzer(String message) {
         this.message = message;
     }
-
     public String analyseMood() {
-        try {
-
-            if (this.message.contains("Sad"))
-                return "SAD";
-            else
-                return "HAPPY";
-        } catch (NullPointerException e) {
+        if (this.message.contains("Sad"))
+            return "SAD";
+        else
             return "HAPPY";
-        }
     }
 }
